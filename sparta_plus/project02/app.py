6fc54b36bf292969f3f5db250463c4dbd1af0efc
Fauzun Naja <bangnaja200@gmail.com>
2023-03-16 20:49:25 +0700

    week 10

--- sparta_plus/project02/app.py
+++ sparta_plus/project02/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, jsonify, request
 import requests
 from pymongo import MongoClient
 from datetime import datetime
+from bson import ObjectId
 
 
 app = Flask(__name__)
@@ -77,6 +78,7 @@ def save_word():
 def delete_word():
     word = request.form.get('word_give')
     db.words.delete_one({'word': word})
+    db.examples.delete_many({'word': word})
 
     return jsonify({
         'result': 'success',
@@ -94,6 +96,64 @@ def error():
         word=word,
         suggestions=suggestions
     )
+
+
+@app.route('/api/get_exs', methods=['GET'])
+def get_exs():
+    word = request.args.get('word')
+    example_data = db.examples.find({'word': word})
+    examples = []
+    for example in example_data:
+        examples.append({
+            'example': example.get('example'),
+            'id': str(example.get('_id'))
+        })
+
+    return jsonify({
+        "result": "success",
+        "examples": examples
+    })
+
+@app.route('/api/save_ex', methods=['POST'])
+def save_ex():
+    word = request.form.get('word')
+    example = request.form.get('example')
+    doc = {
+        'word': word,
+        'example': example
+    }
+
+    db.examples.insert_one(doc)
+    return jsonify({
+        'result': 'success',
+        'msg': f'Your example, {example}, for the word, {word}, was saved!'
+    })
+
+
+@app.route('/api/delete_ex', methods=['POST'])
+def delete_ex():
+    id = request.form.get('id')
+    word = request.form.get('word')
+    db.examples.delete_one({'_id': ObjectId(id)})
+
+    return jsonify({
+        'result': 'success',
+        'msg': f'Your example for the word, {word}, was deleted!',
+    })
+
+
+
+# @app.route('/error-lia')
+# def error():
+#     word = request.args.get('word')
+#     suggestions = request.args.get('suggestions')
+#     if suggestions:
+#         suggestions = suggestions.split(',')
+#     return render_template(
+#         'error.html', 
+#         word=word,
+#         suggestions=suggestions
+#     )
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
