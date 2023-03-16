e01ed3824f04c9fdd4140ad597e3919882e2b1db
Fauzun Naja <bangnaja200@gmail.com>
2023-03-17 00:12:53 +0700

    week 11

---
+++ sparta_plus/project03/app.py
@@ -0,0 +1,24 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    jsonify,
+    redirect,
+    url_for
+)
+
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+password = 'kamukepodeh'
+cxn_str = f'mongodb+srv://fauzunnaja:{password}@cluster0.kwi1dlp.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(cxn_str)
+db = client.dbsparta
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
