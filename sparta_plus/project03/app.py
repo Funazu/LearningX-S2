7cc7cf3a806f4d0e1537012cc7c856551870cbd0
Fauzun Naja <bangnaja200@gmail.com>
2023-03-17 08:36:16 +0700

    week 12

--- sparta_plus/project03/app.py
+++ sparta_plus/project03/app.py
@@ -20,5 +20,9 @@ db = client.dbsparta
 def main():
     return render_template('index.html')
 
+@app.route('/map')
+def map_example():
+    return render_template('prac_map.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
